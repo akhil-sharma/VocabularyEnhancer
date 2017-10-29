fa491678a6b263bc918d9d70482b8468a3046a5a
Akhil Sharma <akhilsharma.cse@gmail.com>
2017-10-29 10:12:06 +0530

    Update cms.php

--- cms.php
+++ cms.php
@@ -98,8 +98,13 @@ $conn = new mysqli($host, $username, $password, $db);
 
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
+    die("Connection failed: " . $conn->connect_error);?>
+
+<script>
+    console.log("connection made!");
+</script>
+
+<?php } 
 echo "connection made";
 
 $sql = "INSERT INTO `main` (Word, Meaning) VALUES ('$word', '$meaning')";
